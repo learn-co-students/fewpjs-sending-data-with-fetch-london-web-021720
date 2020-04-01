0e4c4f1e8190405f875e29e4398a56c4fbb7ecbd
Amaterasus <skyetay03@googlemail.com>
2020-04-01 10:34:39 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email)
+{
+    const data = {
+        name: name,
+        email: email
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+            .then(responce => responce.json())
+            .then(object => document.body.innerHTML = object.id)
+            .catch(error => document.body.innerHTML = error.message)
+}
