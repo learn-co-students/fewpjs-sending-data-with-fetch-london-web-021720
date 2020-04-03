368b31a30f1778ae79451af598d18ff0457775d6
nickzaf95 <nicholaszafiriou@gmail.com>
2020-04-03 14:08:01 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+            })
+        })
+    .then( response => response.json() )
+    .then( object => {
+        let extra = document.querySelector("body")
+        let extraText = `The ID is ${object.id}`
+        extra.innerHTML = extraText
+    } )
+    .catch(function(error) {
+        alert("ERROR");
+        let extra = document.querySelector("body")
+        let extraText = `The error is ${error.message}`
+        extra.innerHTML = extraText
+      });
+}
+
